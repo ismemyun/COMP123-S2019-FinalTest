02edfa87da53ad5d013a4c4a30b394916d051fe1
Yun <cyang91@my.centennialcollege.ca>
2019-08-12 14:54:25 -0400

    added several class

---
+++ COMP123-S2019-FinalTestC/Objects/Identity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Student name:
+ * Student Id:
+ * description: This is the Identity Class to be used with the CaracterPortfolio class
+ */
+namespace COMP123_S2019_FinalTestC.Objects
+{
+    public class Identity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ COMP123-S2019-FinalTestC/Objects/Skill.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Student name:
+ * Student Id:
+ * description: This is the Skill Class to be used with the CaracterPortfolio class
+ */
+namespace COMP123_S2019_FinalTestC.Objects
+{
+    public class Skill
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+    }
+}
