fa008d06b3f84f825fd5a05aedcf359583024071
Yun <cyang91@my.centennialcollege.ca>
2019-08-13 18:36:46 -0400

    added TabControl

--- COMP123-S2019-FinalTestC/Views/CharacterGeneratorForm.Designer.cs
+++ COMP123-S2019-FinalTestC/Views/CharacterGeneratorForm.Designer.cs
@@ -46,7 +46,7 @@
             this.MainTabControl.Controls.Add(this.CharacterSheetPage);
             this.MainTabControl.Dock = System.Windows.Forms.DockStyle.Top;
             this.MainTabControl.Location = new System.Drawing.Point(0, 0);
-            this.MainTabControl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MainTabControl.Margin = new System.Windows.Forms.Padding(4);
             this.MainTabControl.Name = "MainTabControl";
             this.MainTabControl.SelectedIndex = 0;
             this.MainTabControl.Size = new System.Drawing.Size(784, 447);
@@ -55,9 +55,9 @@
             // IdentityPage
             // 
             this.IdentityPage.Location = new System.Drawing.Point(4, 32);
-            this.IdentityPage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.IdentityPage.Margin = new System.Windows.Forms.Padding(4);
             this.IdentityPage.Name = "IdentityPage";
-            this.IdentityPage.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.IdentityPage.Padding = new System.Windows.Forms.Padding(4);
             this.IdentityPage.Size = new System.Drawing.Size(776, 411);
             this.IdentityPage.TabIndex = 0;
             this.IdentityPage.Text = "Identity";
@@ -66,38 +66,38 @@
             // AbilityPage
             // 
             this.AbilityPage.Location = new System.Drawing.Point(4, 32);
-            this.AbilityPage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.AbilityPage.Margin = new System.Windows.Forms.Padding(4);
             this.AbilityPage.Name = "AbilityPage";
-            this.AbilityPage.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.AbilityPage.Padding = new System.Windows.Forms.Padding(4);
             this.AbilityPage.Size = new System.Drawing.Size(776, 411);
             this.AbilityPage.TabIndex = 1;
-            this.AbilityPage.Text = "Ability";
+            this.AbilityPage.Text = "Abilities";
             this.AbilityPage.UseVisualStyleBackColor = true;
             // 
             // SkillPage
             // 
             this.SkillPage.Location = new System.Drawing.Point(4, 32);
-            this.SkillPage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.SkillPage.Margin = new System.Windows.Forms.Padding(4);
             this.SkillPage.Name = "SkillPage";
-            this.SkillPage.Size = new System.Drawing.Size(933, 505);
+            this.SkillPage.Size = new System.Drawing.Size(776, 411);
             this.SkillPage.TabIndex = 2;
-            this.SkillPage.Text = "Skill";
+            this.SkillPage.Text = "Skills";
             this.SkillPage.UseVisualStyleBackColor = true;
             // 
             // CharacterSheetPage
             // 
             this.CharacterSheetPage.Location = new System.Drawing.Point(4, 32);
-            this.CharacterSheetPage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.CharacterSheetPage.Margin = new System.Windows.Forms.Padding(4);
             this.CharacterSheetPage.Name = "CharacterSheetPage";
-            this.CharacterSheetPage.Size = new System.Drawing.Size(933, 505);
+            this.CharacterSheetPage.Size = new System.Drawing.Size(776, 411);
             this.CharacterSheetPage.TabIndex = 3;
-            this.CharacterSheetPage.Text = "CharacterSheet";
+            this.CharacterSheetPage.Text = "Character Sheet";
             this.CharacterSheetPage.UseVisualStyleBackColor = true;
             // 
             // BackButton
             // 
             this.BackButton.Location = new System.Drawing.Point(44, 467);
-            this.BackButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BackButton.Margin = new System.Windows.Forms.Padding(4);
             this.BackButton.Name = "BackButton";
             this.BackButton.Size = new System.Drawing.Size(156, 71);
             this.BackButton.TabIndex = 1;
@@ -108,7 +108,7 @@
             // 
             this.NextButton.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.NextButton.Location = new System.Drawing.Point(569, 467);
-            this.NextButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.NextButton.Margin = new System.Windows.Forms.Padding(4);
             this.NextButton.Name = "NextButton";
             this.NextButton.Size = new System.Drawing.Size(156, 71);
             this.NextButton.TabIndex = 2;
@@ -124,7 +124,7 @@
             this.Controls.Add(this.BackButton);
             this.Controls.Add(this.MainTabControl);
             this.Font = new System.Drawing.Font("Verdana", 14F);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "CharacterGeneratorForm";
             this.Text = "Character Generator";
             this.MainTabControl.ResumeLayout(false);
